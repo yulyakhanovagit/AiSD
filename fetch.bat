462840837875e9e9e0c4c4f35fd990b457402a0e
makometr <makometr@yandex.ru>
2018-09-13 21:59:32 +0300

    Update

--- fetch.bat
+++ fetch.bat
@@ -2,3 +2,5 @@ git fetch GabovES
 git fetch AndersonGH
 git fetch GlazunovSA
 git fetch ProtsvetkinaAV
+git fetch IlyasovAV
+git fetch GosudarkinYS
--- remotes.bat
+++ remotes.bat
@@ -2,3 +2,5 @@ git remote add GabovES https://github.com/GabovYegor/AiSD
 git remote add AndersonGH https://github.com/AndersonGH/AiSD
 git remote add GlazunovSA https://github.com/light5551/AiSD
 git remote add ProtsvetkinaAV https://github.com/ana24stasiya/AiSD
+git remote add IlyasovAV https://github.com/IlyasovAnton/AiSD
+git remote add GosudarkinYS https://github.com/DarkFlink/AiSD
