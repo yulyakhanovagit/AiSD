50036a7da6cfb2e6ffa52811fee67c38791c288b
Лящевская Анастасия <31561368+Anastasiyalp@users.noreply.github.com>
2018-09-21 02:45:59 +0300

    [CODE ACCEPT] [TESTS ACCEPT] 7382 LyashchevskayaAP LAB1  (#10)
    * TESTS READY
    * CODE READY

---
+++ 7382/LyashchevskayaAP/Lab1/Compile&exec.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+gcc ./Sourse/lab1.c -o Lab1
+echo -e '_______\nTest 1:'
+cat ./Tests/Test1.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test1.txt
+echo -e ''
+echo -e '_______\nTest 2:'
+cat ./Tests/Test2.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test2.txt
+echo -e ''
+echo -e '_______\nTest 3:'
+cat ./Tests/Test3.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test3.txt
+echo -e ''
+echo -e '_______\nTest 4:'
+cat ./Tests/Test4.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test4.txt
+echo -e ''
+echo -e '_______\nTest 5:'
+cat ./Tests/Test5.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test5.txt
+echo -e ''
+echo -e '_______\nTest 6:'
+cat ./Tests/Test6.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test6.txt
+echo -e ''
+echo -e '_______\nTest 7:'
+cat ./Tests/Test7.txt
+echo -e '_______\nTesting:\n'
+./Lab1 < ./Tests/Test7.txt
+
---
+++ 7382/LyashchevskayaAP/Lab1/Sourse/lab1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int rec(int indent, int length, int *vector, int *vectorNew){
+	for(int i = 0; i < indent; i++)	printf("\t");			//Print the desired number of indents
+	printf("> rec with vector: ");
+	for(int i = 0; i < length; i++)	printf("%d ", vector[i]);	//Print current vector
+	printf("\n");
+
+	int halfl = length/2;
+	if(length > 2)							//Case of using functions in a function
+		rec(indent + 1, length - halfl, vector, vectorNew) &
+		rec(indent + 1, halfl, vector + length - halfl, vectorNew + length - halfl);
+	if(length == 2)							//Case of distribution of two vectors in ascending order
+		(vectorNew[0] = (vector[0] <= vector[1]) ? vector[0] : vector[1]) &
+		(vectorNew[1] = (vector[0] <= vector[1]) ? vector[1] : vector[0]);
+	if(length == 1)							//Case of ordinary overwriting the vector
+		vectorNew[0] = vector[0];
+
+	for(int i = 0; i < indent; i++)	printf("\t");			//Print the desired number of indents
+	printf("< rec\n");
+}
+
+int main(){
+	char c;
+	printf("Enter length your vector:\n");
+	scanf("%c", &c);
+	if(!isdigit(c)){						//Incorrect length value
+		printf("Error, please, enter DIGIT!\n");
+		return 0;
+	}
+	int length = c - '0';
+
+	int *vector = (int *)malloc(length * sizeof(int));		//The memory allocation of the initial vector
+	int *vectorNew = (int *)malloc(length * sizeof(int));		//and changed vector
+
+	printf("Enter your vector, like example: <1 2 3 4 5>. Without brackets! Only digits!\n");
+	for(int i = 0; i < length; i++)					//Reading a vector
+		scanf("%d", &vector[i]);
+
+	rec(0, length, vector, vectorNew);
+
+	printf("Changed vector look like this:\n");
+	for(int i = 0; i < length; i++)					//Print chandged vector
+		printf("%d ", vectorNew[i]);
+	printf("\n");
+	return 0;
+}
