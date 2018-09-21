64a05e1f3931c46a23440f3268a9c23e72f4434a
Yaroslav Adamov <32567078+yaroslavadamov@users.noreply.github.com>
2018-09-21 12:15:01 +0300

    [CODE FIXED] [TESTS ACCEPT] 7381 AdamovYW lab1 (#16)
    * Adamov lr1 done
    
    * add report

---
+++ 7381/AdamovYW/lab1/Source/main.c
@@ -0,0 +1,85 @@
+﻿#include  <stdio.h>
+
+// сообщения об ошибках
+void error_message(int numb_of_error){
+    printf("\nОшибка: ");
+    switch (numb_of_error){
+        case 1:
+            printf("вы ввели отрицательное значение.\n");
+            break;
+        case 2:
+            printf("m > n\n");
+            break;
+        case 3:
+            printf("необходима ввести целое число.\n");
+            break;
+    }
+    printf("Программа завершила работу.\n\n");
+}
+
+
+// поиск значения биноманального коэфициента рекурсивным способом
+int binominal_coef(int coef_m , int coef_n, int level){
+    for(int i = 0; i < level; i++) // создание отступов
+        printf("    ");
+    printf("call: m = %d, n = %d ; ", coef_m, coef_n); // сообщение о вызове функции
+    if (coef_m == 0 || coef_m == coef_n){
+        printf("return 1\n"); // самый глубокий уровень, возврат единицы
+        return 1;
+    } else if (coef_m > coef_n){
+        printf("return 0\n"); // случай, возможный только при вводе некорректных данных
+        return 1;
+    } else {
+        // вызов рекурсии в случае, если m < n
+        printf("return C(%d,%d) + C(%d,%d)\n", coef_m-1, coef_n-1, coef_m, coef_n-1);
+        return binominal_coef(coef_m-1, coef_n-1, level+1) + binominal_coef(coef_m, coef_n-1, level+1);
+    }
+}
+
+
+int main(void){
+    
+    int coef_m, coef_n, result;
+    
+    
+    // описание программы
+    printf("\nПрограмма для вычисления биноминальных коэффициентов.\n");
+    printf("Вычисление происходит рекурсивно.\n");
+    printf("\n  m   | 1, если m = 0, n = 0 или m = n >= 0,\n");
+    printf("C   = | 0, если m > n >= 0,\n");
+    printf("  n   | C(m-1, n-1) + C(m, n-1) в остальных случаях.\n");
+    printf("\nЗначения m и n - целые неотрицательные числа.\n\n");
+    
+    
+    // ввод данных
+    printf("Введите значение m : ");
+    if (scanf("%d", &coef_m) == 0){ // ввод и проверка на корректность коэфициента m
+        error_message(3);
+        return 0;
+    }
+    printf("Введите значение n : ");
+    if (scanf("%d", &coef_n) == 0){ // ввод и проверка на корректность коэфициента n
+        error_message(3);
+        return 0;
+    }
+    if (coef_m < 0 || coef_n < 0){ // проверка на отсутствие отрицательных значений
+        error_message(1);
+        return 0;
+    }
+    if (coef_m > coef_n){ // проверка: m не должен превосходить n
+        error_message(2);
+        return 0;
+    }
+    
+    
+    // вывод результата
+    printf("\nХод работы алгоритма:\n\n");
+    result = binominal_coef(coef_m, coef_n, 1); // вызов функции, получение результата
+    // вывод результата
+    printf("\n  %d   \n", coef_m);
+    printf("C   =  %d\n", result);
+    printf("  %d   \n", coef_n);
+    printf("\nПрограмма завершила работу.\n\n");
+    
+    return 0;
+}
---
+++ 7381/AdamovYW/lab1/perform_tests.sh
@@ -0,0 +1,17 @@
+echo "Test 1: input 2, 3"
+./program < Tests/test1.txt
+
+echo "Test 2: input 3, 5"
+./program < Tests/test2.txt
+
+echo "Test 3: input 1, 7"
+./program < Tests/test3.txt
+
+echo "Test 4: input 5, 2"
+./program < Tests/test4.txt
+
+echo "Test 5: input -4, 6"
+./program < Tests/test5.txt
+
+echo "Test 6: input ksj, 10"
+./program < Tests/test6.txt
