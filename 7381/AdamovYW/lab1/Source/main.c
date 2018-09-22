06fc817766934cd84968474fd57531e2812c5c00
Yaroslav Adamov <32567078+yaroslavadamov@users.noreply.github.com>
2018-09-22 14:20:59 +0300

    update main.c

--- 7381/AdamovYW/lab1/Source/main.c
+++ 7381/AdamovYW/lab1/Source/main.c
@@ -8,9 +8,6 @@ void error_message(int numb_of_error){
             printf("вы ввели отрицательное значение.\n");
             break;
         case 2:
-            printf("m > n\n");
-            break;
-        case 3:
             printf("необходима ввести целое число.\n");
             break;
     }
@@ -23,16 +20,20 @@ int binominal_coef(int coef_m , int coef_n, int level){
     for(int i = 0; i < level; i++) // создание отступов
         printf("    ");
     printf("call: m = %d, n = %d ; ", coef_m, coef_n); // сообщение о вызове функции
-    if (coef_m == 0 || coef_m == coef_n){
+    if ( (coef_m == 0 && coef_n > 0) || (coef_m == coef_n && coef_n >= 0) ){
         printf("return 1\n"); // самый глубокий уровень, возврат единицы
         return 1;
-    } else if (coef_m > coef_n){
-        printf("return 0\n"); // случай, возможный только при вводе некорректных данных
-        return 1;
+    } else if (coef_m > coef_n && coef_n >= 0){
+        printf("return 0\n"); // случай, возможный только при вводе m > n
+        return 0;
     } else {
         // вызов рекурсии в случае, если m < n
         printf("return C(%d,%d) + C(%d,%d)\n", coef_m-1, coef_n-1, coef_m, coef_n-1);
-        return binominal_coef(coef_m-1, coef_n-1, level+1) + binominal_coef(coef_m, coef_n-1, level+1);
+        int result = binominal_coef(coef_m-1, coef_n-1, level+1) + binominal_coef(coef_m, coef_n-1, level+1);
+        for(int i = 0; i < level; i++)
+            printf("    ");
+        printf("C(%d,%d) + C(%d,%d) = %d\n", coef_m-1, coef_n-1, coef_m, coef_n-1, result);
+        return result;
     }
 }
 
@@ -54,20 +55,16 @@ int main(void){
     // ввод данных
     printf("Введите значение m : ");
     if (scanf("%d", &coef_m) == 0){ // ввод и проверка на корректность коэфициента m
-        error_message(3);
+        error_message(2);
         return 0;
     }
     printf("Введите значение n : ");
     if (scanf("%d", &coef_n) == 0){ // ввод и проверка на корректность коэфициента n
-        error_message(3);
+        error_message(2);
         return 0;
     }
     if (coef_m < 0 || coef_n < 0){ // проверка на отсутствие отрицательных значений
         error_message(1);
-        return 0;
-    }
-    if (coef_m > coef_n){ // проверка: m не должен превосходить n
-        error_message(2);
         return 0;
     }
     
