b90f3827a8c30e981ea90081de1566b604749b93
SD-SanaZI <31595916+SD-SanaZI@users.noreply.github.com>
2018-11-08 02:36:42 +0300

    Lab3 Done (#107)

---
+++ 7382/DrozdAS/lab3/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bush
+gcc ~/Lab3/Source/main.c -o lab3
---
+++ 7382/DrozdAS/lab3/test.sh
@@ -0,0 +1,41 @@
+gcc ./Sourse/main.c
+echo -e '_______\nTest 1:'
+cat ./Tests/test1.txt 
+echo -e '_______\nTesting:\n'
+./a.out < Tests/test1.txt 
+echo -e ''
+echo -e '_______\nTest 2:'
+cat ./Tests/test2.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test2.txt 
+echo -e ''
+echo -e '_______\nTest 3:'
+cat ./Tests/test3.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test3.txt 
+echo -e ''
+echo -e '_______\nTest 4:'
+cat ./Tests/test4.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test4.txt 
+echo -e ''
+echo -e '_______\nTest 5:'
+cat ./Tests/test5.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test5.txt 
+echo -e ''
+echo -e '_______\nTest 6:'
+cat ./Tests/test6.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test6.txt 
+echo -e ''
+echo -e '_______\nTest 7:'
+cat ./Tests/test7.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test7.txt 
+echo -e ''
+echo -e '_______\nTest 8:'
+cat ./Tests/test8.txt 
+echo -e '_______\nTesting:\n'
+./a.out < ./Tests/test8.txt 
+
